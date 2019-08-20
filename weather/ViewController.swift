c2cce9e3f16af5d2a808e9be2f99374ca4f0dbe1
Sergey Vasilkov <sergeyvasilkov23@gmail.com>
2019-08-20 15:38:59 +0600

    start

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wWather
+//
+//  Created by Сергей on 20/08/2019.
+//  Copyright © 2019 Effective. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
